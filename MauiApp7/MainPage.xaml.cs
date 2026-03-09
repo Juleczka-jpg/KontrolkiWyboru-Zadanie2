983e6b8d62b5ea2a58d4d2825266abeca6ada698
Juleczka <nowjulia12@gmail.com>
2026-03-09 11:07:33 +0100

    Dodaj pliki projektów.

---
+++ MauiApp7/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp7
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Specializations { get; set; }
+        public DateTime MinDate { get; set; }
+        public DateTime MaxDate { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Specializations = new ObservableCollection<string>
+            {
+                "Internista",
+                "Kardiolog",
+                "Dermatolog",
+                "Okulista"
+            };
+
+            MinDate = DateTime.Today.AddDays(1);
+            MaxDate = DateTime.Today.AddDays(30);
+
+            BindingContext = this;
+        }
+        private async void OnReserveClicked(object sender, EventArgs e)
+        {
+            if (SpecializationPicker.SelectedItem == null)
+            {
+                await DisplayAlert("Błąd", "Wybierz specjalizację.", "OK");
+                return;
+            }
+
+            var specialization = SpecializationPicker.SelectedItem.ToString();
+            var date = VisitDatePicker.Date.ToShortDateString();
+            var time = VisitTimePicker.Time.ToString(@"hh\:mm");
+
+            await DisplayAlert("Potwierdzenie",
+                $"Zarezerwowano wizytę u: {specialization}\nData: {date}\nGodzina: {time}",
+                "OK");
+        }
+
+    }
+}
